ce27e47d8d2065dcfc5e2bc85f94ef54c22f392a
rndmemex <rndmemex@protonmail.com>
2020-12-28 18:45:24 +0000

    my answer

--- my_answers/Lesson5/Project/licence_plate_recognition.py
+++ my_answers/Lesson5/Project/licence_plate_recognition.py
@@ -1,3 +1,18 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import imutils
+import pytesseract
+
+def show_image(image, cmap = None, fig_size = (10, 10)):
+    fig, ax = plt.subplots(figsize=fig_size)
+    ax.imshow(image, cmap = cmap)
+    ax.axis('on')
+    plt.show()
+
+
+
+
 image = cv2.imread('../img/licence_plate_raw.png')
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
@@ -5,7 +20,7 @@ plt.figure(figsize=(20,10))
 plt.subplot(2,2,1),plt.imshow(gray, cmap = 'gray')
 plt.title('Original'), plt.xticks([]), plt.yticks([])
 
-Kernel_size= (11,11)
+kernel_size= (11,11)
 
 # loop over the kernels and apply a "closing" operation to the image
 kernel = cv2.getStructuringElement(cv2.MORPH_RECT, kernel_size)
@@ -24,6 +39,7 @@ plt.show()
 (topx, topy) = (np.min(x), np.min(y))
 (bottomx, bottomy) = (np.max(x), np.max(y))
 Cropped = masked[topx:bottomx+1, topy:bottomy+1]
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 plt.figure(figsize=(20,10))
 plt.subplot(221),plt.imshow(Cropped, cmap = 'gray')
 plt.show()
